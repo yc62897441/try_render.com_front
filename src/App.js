0b47211626f9c8ea12a4b4e6386f3d6b6d3a1282
unknown <yc62897441@gmail.com>
2024-01-26 11:50:37 +0800

    style: 加入基礎 CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+// 套件
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// 自定義 components
 import HomePage from './containers/HomePage.jsx'
+
+// style
+import './index.css'
 
 function App() {
     return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
