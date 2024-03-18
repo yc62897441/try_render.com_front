9e64f75e01c775d80073f6a1a77fc48facbc581b
unknown <yc62897441@gmail.com>
2024-03-18 14:51:57 +0800

    feat: 測試 google login api

--- src/containers/LoginPage.jsx
+++ src/containers/LoginPage.jsx
@@ -61,6 +61,48 @@ const forgetPasswordConfig = {
     },
 }
 
+async function oauthSignIn() {
+    try {
+        // Google's OAuth 2.0 endpoint for requesting an access token
+        var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
+
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        var form = document.createElement('form')
+        form.setAttribute('method', 'GET') // Send as a GET request.
+        form.setAttribute('action', oauth2Endpoint)
+
+        console.log(
+            'process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID',
+            process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID
+        )
+
+        // Parameters to pass to OAuth 2.0 endpoint.
+        var params = {
+            client_id: process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID,
+            redirect_uri: process.env.REACT_APP_GOOGLE_LOGIN_REDIRECT_URI,
+            response_type: 'token',
+            scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
+            include_granted_scopes: 'true',
+            state: 'pass-through value',
+        }
+
+        // Add form parameters as hidden input values.
+        for (var p in params) {
+            var input = document.createElement('input')
+            input.setAttribute('type', 'hidden')
+            input.setAttribute('name', p)
+            input.setAttribute('value', params[p])
+            form.appendChild(input)
+        }
+
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form)
+        form.submit()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 function FormWrapper({ formType, changeFormType, formConfig, reCaptchaSubmit }) {
     const dispatch = useDispatch()
     const [formData, setFormData] = useState({}) // 帳號a001 密碼abc123
@@ -149,6 +191,10 @@ function FormWrapper({ formType, changeFormType, formConfig, reCaptchaSubmit }) 
                 AppendComponent={function AppendComponent() {
                     return (
                         <div className='formControlGroup'>
+                            {formType === 'login' && (
+                                <div onClick={oauthSignIn}>使用 Google login</div>
+                            )}
+
                             {formType !== 'login' && (
                                 <div onClick={() => changeFormType('login')}>登入</div>
                             )}
@@ -252,55 +298,14 @@ function LoginPage() {
             break
     }
 
-    async function oauthSignIn() {
-        try {
-            // Google's OAuth 2.0 endpoint for requesting an access token
-            var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth'
-
-            // Create <form> element to submit parameters to OAuth 2.0 endpoint.
-            var form = document.createElement('form')
-            form.setAttribute('method', 'GET') // Send as a GET request.
-            form.setAttribute('action', oauth2Endpoint)
-
-            // Parameters to pass to OAuth 2.0 endpoint.
-            var params = {
-                client_id: process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID,
-                redirect_uri: process.env.REACT_APP_GOOGLE_LOGIN_REDIRECT_URI,
-                response_type: 'token',
-                scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
-                include_granted_scopes: 'true',
-                state: 'pass-through value',
-            }
-
-            // Add form parameters as hidden input values.
-            for (var p in params) {
-                var input = document.createElement('input')
-                input.setAttribute('type', 'hidden')
-                input.setAttribute('name', p)
-                input.setAttribute('value', params[p])
-                form.appendChild(input)
-            }
-
-            // Add form to page and submit it to open the OAuth 2.0 endpoint.
-            document.body.appendChild(form)
-            form.submit()
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    // 如果是從 google OAuth2 redirect 回來，則須從 redirect url 解析回回傳的參數
+    // 如果是從 google OAuth2 redirect 回來，則須從 redirect url 解析回傳的參數
     if (location.href.includes('state')) {
         const paramsPart = location.href.split('/?')[1]
-        // console.log('location.href paramsPart', paramsPart)
-
         const table = {}
         paramsPart.split('&').forEach((pair) => {
-            console.log(pair.split('='))
             const [key, value] = pair.split('=')
             table[key] = value
         })
-        // console.log('table', table)
 
         // 使用回傳參數 access_token，去向 google API 取回使用者資訊
         fetchGoogleUserInfo(table.access_token)
@@ -365,8 +370,7 @@ function LoginPage() {
 
                 {/* LOGO */}
                 <div className='loginLogoWrapper'>
-                    {/* <img src={LogoImg} alt='貓咪 icon' srcSet='' /> */}
-                    <div onClick={oauthSignIn}>Google login</div>
+                    <img src={LogoImg} alt='貓咪 icon' srcSet='' />
                 </div>
             </section>
         </main>
