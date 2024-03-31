2f7b3d583bcc088821c0c5de564e55f88907afc5
yc62897441 <yc62897441@gmail.com>
2024-03-31 08:46:22 +0800

    feat: 如果有貓咪 ids 在訂單的起訖時間內無法預定，則畫面顯示錯誤提示

--- src/config/containers/cartPage.js
+++ src/config/containers/cartPage.js
@@ -1,35 +1,35 @@
 import { cityList, cityDistrictList } from '../cityAndDistrict'
 
 export const cartPageFormConfig = {
-    // email: {
-    //     label: 'email',
-    //     type: 'email',
-    //     required: true,
-    // },
-    // tel: {
-    //     label: '手機',
-    //     type: 'tel',
-    //     required: true,
-    //     pattern: '[0-9]{4}-[0-9]{3}-[0-9]{3}',
-    //     patternDemo: ' Format: 0900-001-001',
-    // },
-    // city: {
-    //     label: '居住縣市',
-    //     type: 'select',
-    //     selectList: cityList,
-    //     required: true,
-    // },
-    // district: {
-    //     label: '行政區',
-    //     type: 'selectDistrict',
-    //     selectList: cityDistrictList,
-    //     required: true,
-    // },
-    // address: {
-    //     label: '地址',
-    //     type: 'text',
-    //     required: true,
-    // },
+    email: {
+        label: 'email',
+        type: 'email',
+        required: true,
+    },
+    tel: {
+        label: '手機',
+        type: 'tel',
+        required: true,
+        pattern: '[0-9]{4}-[0-9]{3}-[0-9]{3}',
+        patternDemo: ' Format: 0900-001-001',
+    },
+    city: {
+        label: '居住縣市',
+        type: 'select',
+        selectList: cityList,
+        required: true,
+    },
+    district: {
+        label: '行政區',
+        type: 'selectDistrict',
+        selectList: cityDistrictList,
+        required: true,
+    },
+    address: {
+        label: '地址',
+        type: 'text',
+        required: true,
+    },
     startDate: {
         label: '起日',
         type: 'date',
